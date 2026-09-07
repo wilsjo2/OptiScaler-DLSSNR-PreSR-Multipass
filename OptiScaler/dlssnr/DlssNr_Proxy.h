2e7488d2993ad12b03a04a53e8e5bfb740d7bf1f
drwrf44 <b3229411195@gmail.com>
2026-09-07 19:55:41 +0700

    test

--- OptiScaler/dlssnr/DlssNr_Proxy.h
+++ OptiScaler/dlssnr/DlssNr_Proxy.h
@@ -43,8 +43,9 @@ bool Available();
 unsigned int Run(ID3D12GraphicsCommandList* cmdList, ID3D12Device* device, ID3D12Resource* color,
                  ID3D12Resource* depth, ID3D12Resource* motion, ID3D12Resource* output,
                  unsigned int width, unsigned int height, unsigned int guideWidth,
-                 unsigned int guideHeight, bool depthInverted, bool reset, float mvScaleX,
-                 float mvScaleY);
+                 unsigned int guideHeight, unsigned int motionWidth, unsigned int motionHeight,
+                 unsigned int depthBaseX, unsigned int depthBaseY, unsigned int motionBaseX,
+                 unsigned int motionBaseY, bool depthInverted, bool reset, float mvScaleX, float mvScaleY);
 
 // Drops the feature and its parameter block, for a resolution change or shutdown.
 void Release();
